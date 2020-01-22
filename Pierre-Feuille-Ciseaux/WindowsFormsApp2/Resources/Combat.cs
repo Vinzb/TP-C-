374549e24a0bdfe065aebf6e74026bf7e509ca53
Vinzb <32279745+Vinzb@users.noreply.github.com>
2020-01-22 09:31:21 +0100

    Add files via upload

---
+++ Pierre-Feuille-Ciseaux/WindowsFormsApp2/Resources/Combat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.Resources
+{
+    class Combat
+    {
+        public int aleatoire;
+
+        public Combat()
+        {
+            Random alea = new Random();
+            aleatoire = alea.Next(1, 3);
+        }
+    }
+
+}
+
